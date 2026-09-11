f5ef2a4da8339814515133d7df4d1c738b2a4a97
Muhammad Usama <usama.webdev54@gmail. git config --global user.email usama.webdev54@gmail.>
2026-09-11 19:13:45 +0500

    save

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php', // <-- Yeh line yahan add karni hai
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
